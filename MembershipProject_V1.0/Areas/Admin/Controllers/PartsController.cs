0afa559e1418bc5759c058c28ad0ed83946608ab
tgaud1961 <tgauden@outlook.com>
2017-04-23 09:46:06 -0400

    Added Part Data Views
    Added Part Data views and styling

---
+++ MembershipProject_V1.0/Areas/Admin/Controllers/PartsController.cs
@@ -0,0 +1,124 @@
+﻿using MembershipProject_V1._0.Entities;
+using MembershipProject_V1._0.Models;
+using System.Data.Entity;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace MembershipProject_V1._0.Areas.Admin.Controllers
+{
+    public class PartsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Admin/Parts
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Parts.ToListAsync());
+        }
+
+        // GET: Admin/Parts/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Part part = await db.Parts.FindAsync(id);
+            if (part == null)
+            {
+                return HttpNotFound();
+            }
+            return View(part);
+        }
+
+        // GET: Admin/Parts/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Parts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Title")] Part part)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Parts.Add(part);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(part);
+        }
+
+        // GET: Admin/Parts/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Part part = await db.Parts.FindAsync(id);
+            if (part == null)
+            {
+                return HttpNotFound();
+            }
+            return View(part);
+        }
+
+        // POST: Admin/Parts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Title")] Part part)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(part).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(part);
+        }
+
+        // GET: Admin/Parts/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Part part = await db.Parts.FindAsync(id);
+            if (part == null)
+            {
+                return HttpNotFound();
+            }
+            return View(part);
+        }
+
+        // POST: Admin/Parts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Part part = await db.Parts.FindAsync(id);
+            db.Parts.Remove(part);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
