b479002e70311a98dc21a856fe854e578fa02fd3
tgaud1961 <tgauden@outlook.com>
2017-04-23 08:30:49 -0400

    Added entity classes

---
+++ MembershipProject_V1.0/Entities/Item.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Web.Mvc;
+
+namespace MembershipProject_V1._0.Entities
+{
+    [Table("Item")]
+    public class Item
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [MaxLength(255)]
+        [Required]
+        public string Title { get; set; }
+
+        [MaxLength(2048)]
+        public string Description { get; set; }
+
+        [MaxLength(1024)]
+        public string Url { get; set; }
+
+        [MaxLength(1024)]
+        public string ImageUrl { get; set; }
+
+        [AllowHtml]
+        public string HTML { get; set; }
+
+        [DefaultValue(0)]
+        public int WaitDays { get; set; }
+
+        public string HTMLShort
+        {
+            get
+            {
+                return HTML == null || HTML.Length < 50 ? HTML : HTML.Substring(0, 50);
+            }
+        }
+
+        public int ProductId { get; set; }
+
+        public int ItemTypeId { get; set; }
+
+        public int SectionId { get; set; }
+
+        public int PartId { get; set; }
+
+        public int IsFree { get; set; }
+
+        [DisplayName("Item Types")]
+        public ICollection<ItemType> ItemTypes { get; set; }
+
+        [DisplayName("Sections")]
+        public ICollection<Section> Sections { get; set; }
+
+        [DisplayName("Parts")]
+        public ICollection<Part> Parts { get; set; }
+    }
+}
