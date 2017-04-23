53a0588e9666f6a8a1d14c6703406700c75b769f
tgaud1961 <tgauden@outlook.com>
2017-04-22 20:33:20 -0400

    New Project Start
    Adding Project directory

---
+++ MembershipProject_V1.0/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MembershipProject_V1._0.Startup))]
+namespace MembershipProject_V1._0
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
